f5ed1c1a69d3fd17255cb641fc3beee666fd5f04
jwqx.pk <jwqx.pk@gmail.com>
2024-04-16 13:01:50 +0200

    Automatic country by IP

--- script.js
+++ script.js
@@ -1,64 +1,71 @@
 let clickCount = 0;
 
-const countryInput = document.getElementById('country');
-const myForm = document.getElementById('form');
-const modal = document.getElementById('form-feedback-modal');
-const clicksInfo = document.getElementById('click-count');
+//const countryInput = document.getElementById("country");
+const countryInput = document.getElementById("country");
+const myForm = document.getElementById("form");
+const modal = document.getElementById("form-feedback-modal");
+const clicksInfo = document.getElementById("click-count");
 
 function handleClick() {
-    clickCount++;
-    clicksInfo.innerText = clickCount;
+  clickCount++;
+  clicksInfo.innerText = clickCount;
 }
 
 async function fetchAndFillCountries() {
-    try {
-        const response = await fetch('https://restcountries.com/v3.1/all');
-        if (!response.ok) {
-            throw new Error('Błąd pobierania danych');
-        }
-        const data = await response.json();
-        const countries = data.map(country => country.name.common);
-        countryInput.innerHTML = countries.map(country => `<option value="${country}">${country}</option>`).join('');
-    } catch (error) {
-        console.error('Wystąpił błąd:', error);
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    if (!response.ok) {
+      throw new Error("Błąd pobierania danych");
     }
+    const data = await response.json();
+    const countries = data.map((country) => country.name.common).sort();
+    countryInput.innerHTML = countries
+      .map((country) => `<option value="${country}">${country}</option>`)
+      .join("");
+  } catch (error) {
+    console.error("Wystąpił błąd:", error);
+  }
 }
 
 function getCountryByIP() {
-    fetch('https://get.geojs.io/v1/ip/geo.json')
-        .then(response => response.json())
-        .then(data => {
-            const country = data.country;
-            // TODO inject country to form and call getCountryCode(country) function
-        })
-        .catch(error => {
-            console.error('Błąd pobierania danych z serwera GeoJS:', error);
-        });
-}
-
-function getCountryCode(countryName) {
-    const apiUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-
-    fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Błąd pobierania danych');
-        }
-        return response.json();
+  fetch("https://get.geojs.io/v1/ip/geo.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const country = data.country;
+      // TODO inject country to form and call getCountryCode(country) function
+      countryInput.value = country;
+      getCountryCode(country);
     })
-    .then(data => {        
-        const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
-        // TODO inject countryCode to form
-    })
-    .catch(error => {
-        console.error('Wystąpił błąd:', error);
+    .catch((error) => {
+      console.error("Błąd pobierania danych z serwera GeoJS:", error);
     });
 }
 
+function getCountryCode(countryName) {
+  const apiUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Błąd pobierania danych");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const countryCode = data[0].idd.root + data[0].idd.suffixes.join("");
+      // TODO inject countryCode to form
+      const countryCodeSelect = document.getElementById("countryCode");
+      countryCodeSelect.value = countryCode;
+    })
+    .catch((error) => {
+      console.error("Wystąpił błąd:", error);
+    });
+}
 
 (() => {
-    // nasłuchiwania na zdarzenie kliknięcia myszką
-    document.addEventListener('click', handleClick);
+  // nasłuchiwania na zdarzenie kliknięcia myszką
+  document.addEventListener("click", handleClick);
 
-    fetchAndFillCountries();
-})()
+  fetchAndFillCountries();
+  getCountryByIP();
+})();
